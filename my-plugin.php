b10b9676b6509b821cad446a0faca39486045513
Arafan Hemho <arafan.h@psu.ac.th>
2025-06-23 15:22:46 +0700

    init

---
+++ my-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: My plugin
+ * Description: This is description of my plugin
+ * Version: 1.0
+ * Author: YOURNAME
+ * Author URI: https://diis.psu.ac.th
+ */
+
+// ป้องกัน direct access
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
